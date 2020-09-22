06130f1b59ce9155be504d1bbd318fb18b21ce33
zedxlucian <lydien-95@hotmail.fr>
2020-09-22 23:40:59 +0200

    Changing font size.

--- config.def.h
+++ config.def.h
@@ -5,10 +5,10 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrains Mono:style=Medium:pixelsize=16:antialias=false:autohint=false";
+static char *font = "JetBrains Mono:style=Medium:pixelsize=14:antialias=false:autohint=false";
 /* Spare fonts */
 static char *font2[] = {
-	"Font Awesome Free 5 Regular:pixelsize=16:antialias=true:autohint=true", 
+	"Font Awesome Free 5 Regular:pixelsize=12:antialias=true:autohint=true", 
 };
 
 static int borderpx = 2;
@@ -99,33 +99,35 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.8, alphaUnfocused = 0.6;
+float alpha = 0.9, alphaUnfocused = 1;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-  /* 8 normal colors */
-  [0] = "#000000", /* black   */
-  [1] = "#ff5555", /* red     */
-  [2] = "#50fa7b", /* green   */
-  [3] = "#f1fa8c", /* yellow  */
-  [4] = "#bd93f9", /* blue    */
-  [5] = "#ff79c6", /* magenta */
-  [6] = "#8be9fd", /* cyan    */
-  [7] = "#bbbbbb", /* white   */
-
-  /* 8 bright colors */
-  [8]  = "#44475a", /* black   */
-  [9]  = "#ff5555", /* red     */
-  [10] = "#50fa7b", /* green   */
-  [11] = "#f1fa8c", /* yellow  */
-  [12] = "#bd93f9", /* blue    */
-  [13] = "#ff79c6", /* magenta */
-  [14] = "#8be9fd", /* cyan    */
-  [15] = "#ffffff", /* white   */
-
-  /* special colors */
-  [256] = "#282a36", /* background */
-  [257] = "#f8f8f2", /* foreground */
+	/* 8 normal colors */
+ 	"black",
+ 	"red3",
+ 	"green3",
+ 	"yellow3",
+ 	"blue2",
+ 	"magenta3",
+ 	"cyan3",
+ 	"gray90",
+ 
+ 	/* 8 bright colors */
+ 	"gray50",
+ 	"red",
+ 	"green",
+ 	"yellow",
+ 	"#5c5cff",
+ 	"magenta",
+ 	"cyan",
+ 	"white",
+ 
+ 	[255] = 0,
+ 
+ 	/* more colors can be added after 255 to use with DefaultXX */
+ 	"#cccccc",
+ 	"#555555",
 };
 
 /*
@@ -133,10 +135,10 @@ static const char *colorname[] = {
  * foreground, background, cursor
  */
 unsigned int defaultfg = 257;
-unsigned int defaultbg = 0;
+unsigned int defaultbg = 256;
 static unsigned int defaultcs = 257;
 static unsigned int defaultrcs = 257;
-unsigned int bg = 17, bgUnfocused = 16;
+unsigned int bg = 256, bgUnfocused = 256;
 
 /*
  * Colors used, when the specific fg == defaultfg. So in reverse mode this
