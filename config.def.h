722553125b6c848f03ed9bfec823cf785503fe75
Lydien SANDANASAMY <s.lydien@icloud.com>
2020-10-06 22:29:33 +0200

    No opacity on active windows, only on non-active windows

--- config.def.h
+++ config.def.h
@@ -99,7 +99,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.9, alphaUnfocused = 1;
+float alpha = 1, alphaUnfocused = 0.9;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
