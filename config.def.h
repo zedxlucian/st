76ff6f6bc52d9e6e3cc0f36163db1a94bfb45df3
Lydien Sandanasamy <s.lydien@icloud.com>
2021-01-03 22:55:05 +0100

    Feature: Add FONT2 config

--- config.def.h
+++ config.def.h
@@ -7,6 +7,12 @@
  */
 static char *font = "JetBrainsMono Nerd Font Mono:pixelsize=12:antialias=true:autohint=true";
 static char *font2 = "JoyPixels:pixelsize=10:antialias=true:autohint=true";
+static char *font2[] = {
+	"JoyPixels:pixelsize=10:antialias=true:autohint=true",
+/*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
+/*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
+};
+
 static int borderpx = 2;
 
 /*
